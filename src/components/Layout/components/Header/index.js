9530c940507450cc971e80da3b1c3eb3a5cc9c13
quanqhuy188 <quanqhuy188@gmail.com>
2022-07-25 19:40:04 +0700

    buildui header part 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,33 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
+
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search account and video" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <div className={cx('loading')}>
+            <FontAwesomeIcon icon={faSpinner} />
+          </div>
+
+          <button className={cx('search-button')}>
+            {' '}
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
+      </div>
     </header>
   );
 }
